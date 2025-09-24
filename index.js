c11f96c0aab108fc300858fbb8c58d51edea2d8e
SirAlex36 <alexngenoo@gmail.com>
2025-09-24 15:43:06 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const mondayWork = function(task = "go to the office") {
+  return(`This Monday, I will ${task}.`);
+}
+function saturdayFun(activity="roller-skate"){
+  return(`This Saturday, I want to ${activity}`+ '!');
+
+}
+function wrapAdjective(flair="*"){
+  return function(adjective){
+    return `You are ${flair}${adjective}${flair}!`;
+  }
+} 
